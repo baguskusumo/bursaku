bfe7e5e77b83e6579845ffab27effcce6c7958f2
Bagus Kusumo Wijonarko <97827298+baguskusumo@users.noreply.github.com>
2022-02-15 20:18:08 -0800

    Add files via upload

---
+++ resources/views/mobil/create.blade.php
@@ -0,0 +1,60 @@
+@extends('template')
+
+@section('content')
+<div class="row mt-5 mb-5">
+    <div class="col-lg-12 margin-tb">
+        <div class="float-left">
+            <h2>Create Data Mobil</h2>
+        </div>
+        <div class="float-right">
+            <a class="btn btn-secondary" href="{{ route('mobil.index') }}"> Kembali</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> Input gagal.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('mobil.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>ID Mobil:</strong>
+                <input type="text" name="id_mobil" class="form-control" placeholder="ID Mobil">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Mesin:</strong>
+                <input type="text" name="mesin" class="form-control" placeholder="Mesin">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Kapasitas Penumpang:</strong>
+                <input type="text" name="kapasitas_penumpang" class="form-control" placeholder="Kapasitas Penumpang">
+            </div>
+        </div>
+		<div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tipe:</strong>
+                <input type="text" name="tipe" class="form-control" placeholder="Tipe">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
