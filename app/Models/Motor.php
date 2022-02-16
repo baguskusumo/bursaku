8a6086ad5448b50a80666324f9c27befff49582e
Bagus Kusumo Wijonarko <97827298+baguskusumo@users.noreply.github.com>
2022-02-15 19:53:02 -0800

    Add files via upload

---
+++ app/Models/Motor.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Motor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'motors';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id_motor','mesin','tipe_suspensi','tipe_transmisi'
+    ];
+}
