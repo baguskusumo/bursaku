daf000d953d7f3b5eb82078160687df6fcf3b6a3
Bagus Kusumo Wijonarko <97827298+baguskusumo@users.noreply.github.com>
2022-02-17 05:16:52 -0800

    Add files via upload

---
+++ resources/views/motor/edit.blade.php
@@ -0,0 +1,73 @@
+@extends('template')
+
+@section('content')
+    <div class="row mt-5 mb-5">
+        <div class="col-lg-12 margin-tb">
+            <div class="float-left">
+                <h2>Edit Data Motor</h2>
+            </div>
+            <div class="float-right">
+                <a class="btn btn-secondary" href="{{ route('motor.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('motor.update',$motor->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>ID Motor:</strong>
+                <input type="text" name="id_motor" class="form-control" placeholder="ID Motor" value="{{ $motor->id_motor }}">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama Motor:</strong>
+                <input type="text" name="nama_motor" class="form-control" placeholder="Nama Motor" value="{{ $motor->nama_motor }}">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Mesin:</strong>
+                <input type="text" name="mesin" value="{{ $motor->mesin }}" class="form-control" placeholder="Mesin">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tipe Suspensi:</strong>
+                <input type="text" name="tipe_suspensi" value="{{ $motor->tipe_suspensi }}" class="form-control" placeholder="Tipe Suspensi">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tipe Transimi:</strong>
+                <input type="text" name="tipe_transmisi" value="{{ $motor->tipe_transmisi }}" class="form-control" placeholder="Tipe Transmisi">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Stock:</strong>
+                <input type="text" name="stock" value="{{ $motor->stock }}" class="form-control" placeholder="Stock">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Update</button>
+        </div>
+    </div>
+
+    </form>
+@endsection
