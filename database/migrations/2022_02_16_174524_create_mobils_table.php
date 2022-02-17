7dd0d9cf4d385edadc36f00af3265db1a5aea947
Bagus Kusumo Wijonarko <97827298+baguskusumo@users.noreply.github.com>
2022-02-17 04:50:24 -0800

    Add files via upload

---
+++ database/migrations/2022_02_16_174524_create_mobils_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMobilsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mobils', function (Blueprint $table) {
+            $table->id();
+            $table->char('id_mobil');
+            $table->text('nama_mobil');
+            $table->text('mesin');
+            $table->text('kapasitas_penumpang');
+            $table->text('tipe');
+            $table->text('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mobils');
+    }
+}
---
+++ database/migrations/2022_02_17_160855_create_detail_pembayarans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailPembayaransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_pembayarans', function (Blueprint $table) {
+            $table->id();
+            $table->text('nama');
+            $table->text('mesin');
+			$table->text('tahun_keluaran');
+            $table->text('warna');
+            $table->text('harga');
+            $table->text('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail__pembayarans');
+    }
+}
