6bd2ac0ef5869011bdaed04a0e9ff7fd677abcca
Bagus Kusumo Wijonarko <97827298+baguskusumo@users.noreply.github.com>
2022-02-17 05:13:56 -0800

    Add files via upload

---
+++ resources/views/bursaku/create.blade.php
@@ -0,0 +1,60 @@
+@extends('template')
+
+@section('content')
+<div class="row mt-5 mb-5">
+    <div class="col-lg-12 margin-tb">
+        <div class="float-left">
+            <h2>Create Data Kendaraan</h2>
+        </div>
+        <div class="float-right">
+            <a class="btn btn-secondary" href="{{ route('bursaku.index') }}"> Kembali</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> Input gagal.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('bursaku.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>ID Kendaraan:</strong>
+                <input type="text" name="id_kendaraan" class="form-control" placeholder="ID Kendaraan">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tahun Keluaran:</strong>
+                <input type="text" name="tahun_keluaran" class="form-control" placeholder="Tahun Keluaran">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Warna:</strong>
+                <input type="text" name="warna" class="form-control" placeholder="Warna">
+            </div>
+        </div>
+		<div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Harga:</strong>
+                <input type="text" name="harga" class="form-control" placeholder="Harga">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
---
+++ resources/views/bursaku/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('template')
+
+@section('content')
+    <div class="row mt-5 mb-5">
+        <div class="col-lg-12 margin-tb">
+            <div class="float-left">
+                <h2>Edit Data Kendaraan</h2>
+            </div>
+            <div class="float-right">
+                <a class="btn btn-secondary" href="{{ route('bursaku.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('bursaku.update',$bursaku->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>ID Kendaraan:</strong>
+                <input type="text" name="id_kendaraan" class="form-control" placeholder="ID Kendaraan" value="{{ $bursaku->id_kendaraan }}">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tahun Keluaraan:</strong>
+                <input type="text" name="tahun_keluaran" value="{{ $bursaku->tahun_keluaran }}" class="form-control" placeholder="Tahun Keluaran">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Warna:</strong>
+                <input type="text" name="warna" value="{{ $bursaku->warna }}" class="form-control" placeholder="Warna">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Harga:</strong>
+                <input type="text" name="harga" value="{{ $bursaku->harga }}" class="form-control" placeholder="Harga">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Update</button>
+        </div>
+    </div>
+
+    </form>
+@endsection
